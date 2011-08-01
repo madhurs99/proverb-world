b8d5bca4a516e55e3e39ecee786ec8c6de791910
madhurs99 <madhurs99@2e702f07-c4d4-491b-9ba9-50e5366e394d>
2011-08-01 18:12:08 +0000

    for language support may change it to map instead plain strings

---
+++ proverbs/src/com/proverbworld/common/Language.java
@@ -0,0 +1,8 @@
+package com.proverbworld.common;
+
+public class Language {
+public static final String PUNJABI ="Punjabi";
+public static final String HINDI="Hindi";
+public static final String MARATHI="Marathi";
+public static final String ENGLISH="English";
+}
